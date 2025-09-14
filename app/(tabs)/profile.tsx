da8f476debb9403e8840d121dc1e437b009d6029
Aditya Raj <adityakarn06@gmail.com>
2025-09-14 14:39:55 +0530

    starter-files

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-dark-200">
+        Profile Page!
+      </Text>
+    </View>
+  )
+}
+
+export default profile
